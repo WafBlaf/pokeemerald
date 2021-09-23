c8d500ec228ba731cced1e7b1527df3d47ba6dca
Pokestia <faiskiru@gmail.com>
2021-09-23 07:56:24 +0200

    Set running shoes flag unused

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1453,6 +1453,7 @@
 #define FLAG_LANDMARK_SKY_PILLAR                    (SYSTEM_FLAGS + 0x5E)
 
 #define FLAG_SYS_SHOAL_ITEM                         (SYSTEM_FLAGS + 0x5F)
+#define FLAG_SYS_UNUSED                             (SYSTEM_FLAGS + 0x60) //Used to be running shoes
 #define FLAG_SYS_CTRL_OBJ_DELETE                    (SYSTEM_FLAGS + 0x61)
 #define FLAG_SYS_RESET_RTC_ENABLE                   (SYSTEM_FLAGS + 0x62)
 
